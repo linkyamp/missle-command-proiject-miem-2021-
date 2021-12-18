f6079a1c00b300574145f38374ff572b83613047
linkyamp <90451761+linkyamp@users.noreply.github.com>
2021-12-18 14:50:25 +0300

    Update collision_test.py

--- collision_test.py
+++ collision_test.py
@@ -4,12 +4,17 @@ from collision import puli
 
 import pygame
 import random
-Rect1=Rect2=pygame.Rect(random.randint(10, 1000), 0, 40, 40)
-Rect3=pygame.Rect(100001, 0, 40, 40)
+Rect1 = Rect2 = pygame.Rect(random.randint(10, 1000), 0, 40, 40)
+Rect3 = pygame.Rect(100001, 0, 40, 40)
 
 
 class TestPuli(unittest.TestCase):
     def test_puli(self):
-        self.assertEqual([Rect1, Rect2, random.randint(100, 1000), random.randint(100, 1000)], 1)
+        self.assertEqual(puli(Rect1, Rect2, random.randint(100, 1000), random.randint(100, 1000)), 1)
+
     def test_promah(self):
-        self.assertEqual([Rect1, Rect3, random.randint(100, 1000), random.randint(100, 1000)], 0)
+        self.assertEqual(promah(Rect1, Rect3, random.randint(100, 1000), random.randint(100, 1000)), 0)
+
+
+if __name__ == '__name__':
+    unittest.main()
