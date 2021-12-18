46744461e022d6ea9604a2f7e24fa09e0318f550
linkyamp <90451761+linkyamp@users.noreply.github.com>
2021-12-18 15:30:26 +0300

    Update game.py

--- game.py
+++ game.py
@@ -32,14 +32,14 @@ rockethitbox3 = pygame.Rect(random.randint(10, 1000), -10000, 40, 40)
 rockethitbox4 = pygame.Rect(random.randint(10, 1000), -40000, 40, 40)
 
 def upravlenie (keys, rocketlauncher):
-    '''Функция, отвечающая за управление. Берёт на вход данные с клавиш(keys), полученные с помощью pygame.key.get_pressed() и также объект класса Rect (rocketlauncher), котороый и будет перемещаться со скоростью, заданной в переменной speed '''
+    '''Функция, отвечающая за управление. Берёт на вход данные с клавиш(keys), полученные с помощью pygame.key.get_pressed() и также объект класса Rect (rocketlauncher), котороый и будет перемещаться со скоростью, равной переменной заданной в коде ранее - speed '''
     if keys[pygame.K_LEFT] and rocketlauncher.x - speed > 0:  # управление - кнопки влево и вправо. второе условие нужно для того, чтобы не выходить за границы экрана
         rocketlauncher.x -= speed
     if keys[pygame.K_RIGHT] and rocketlauncher.x + speed < shir - sootnosheniyakuba:  # если не вычесть длинну самой ракетницы, доходить до конца не будет
         rocketlauncher.x += speed
 
 def vuvodisobrajeniya():
-    '''Функция отрисовки всего(фона, числа очков, вражеских ракет,города, пушки), также отвечает за падение ракет. Требует кучу переменных, перечислять которые нет смысла'''
+    '''Функция отрисовки всего(фона, числа очков, вражеских ракет,города, пушки), также отвечает за падение ракет. Требует кучу переменных, прописанных до этого в коде: цвета - vecherneenebo, white, black; переменная score и соответсвующие приготовления, связанные с выводом текста; загруженный спрайт rocket и 5 итераций его хитбоксов rockethitbox(1,2,3,4) - по сущности, хитбоксы просто прямоугольники класса Rect, к которым также относятся dom1 и rocketlauncher'''
     screen.fill(vecherneenebo)  # делаем фон
     scoreout = scores.render(f"Score: {score}", 1, (white))  # рендер числа очков
     screen.blit(scoreout, (10, 10))  #вывод числа очков
@@ -67,9 +67,9 @@ while True: #бесконечный цикл
                 ourrocket = pygame.Rect(rocketlauncher.x-ourrocketsootnoocheniya//2, 100, ourrocketsootnoocheniya, ourrocketsootnoocheniya)
                 if puli(ourrocket, rockethitbox, vuisota, shir)==1:
                     score+=1
-                if puli(ourrocket, rockethitbox1, vuisota, shir)==1:
+                if puli(ourrocket, rockethitbox1, vuisota, shir)==1: 
                     score+=1
-                if puli(ourrocket, rockethitbox2, vuisota, shir)==1:
+                if puli(ourrocket, rockethitbox2, vuisota, shir)==1:   #через фунцию смотрим, произошла ли коллизия ракеты с пулей, если да - добавляем очки
                     score+=1
                 if puli(ourrocket, rockethitbox3, vuisota, shir)==1:
                     score+=1
@@ -94,7 +94,7 @@ while True: #бесконечный цикл
         score -=1
     if promah(rockethitbox1, dom1, vuisota, shir) == 1:
         score -=1
-    if promah(rockethitbox2, dom1, vuisota, shir) == 1:
+    if promah(rockethitbox2, dom1, vuisota, shir) == 1:  #через фунцию смотрим, произошла ли коллизия  города(dom1) c ракетой, если да - убавляем очки
         score -=1
     if promah(rockethitbox3, dom1, vuisota, shir) == 1:
         score -=1
