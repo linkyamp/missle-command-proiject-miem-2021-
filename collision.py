66670877d6ec27c9c69b3343675246589257560b
linkyamp <90451761+linkyamp@users.noreply.github.com>
2021-12-18 13:32:41 +0300

    Add files via upload

---
+++ collision.py
@@ -0,0 +1,19 @@
+import pygame
+import random
+def promah(rockethitbox, dom1, vuisota, shir):
+    '''Аналогичен функции puli'''
+    if rockethitbox.colliderect(dom1):
+        rockethitbox.y -= vuisota
+        rockethitbox.x = random.randint(10, shir-24)
+        return 1
+    else:
+        return 0
+
+def puli(ourrocket, rockethitbox, vuisota, shir):
+    '''Просчитывает коллизию двух объектов, берёт на вход два объекта класса Rect(ourrocket и rockethitbox), возвращвет 1 в случаи коллизи, 0 в обратном , отправляет один из них(rockethitbox) наверх на высоту окна и со случайной координатой х'''
+    if rockethitbox.colliderect(ourrocket):
+        rockethitbox.y -= vuisota
+        rockethitbox.x = random.randint(10, shir-24)
+        return 1
+    else:
+        return 0
---
+++ game_test.py
@@ -0,0 +1,12 @@
+import unittest
+from sums_test import promah
+from sums_test import puli
+
+import pygame
+import random
+
+
+
+class TestPuli(unittest.TestCase):
+    def test_puli(self):
+        self.assertEqual(puli[pygame.Rect(random.randint(10, 1000), 0, 40, 40), pygame.Rect(random.randint(10, 1000), 0, 40, 40), random.randint(100, 1000), random.randint(100, 1000)], 0)
